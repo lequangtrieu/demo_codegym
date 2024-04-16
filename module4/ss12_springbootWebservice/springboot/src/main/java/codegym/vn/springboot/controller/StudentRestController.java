e8088ceaee2eb143f6e268e76659952aeceab959
lequangtrieu <trieulqde160447@gmail.com>
2024-04-16 14:29:43 +0700

    up

---
+++ module4/ss12_springbootWebservice/springboot/src/main/java/codegym/vn/springboot/controller/StudentRestController.java
@@ -0,0 +1,49 @@
+package codegym.vn.springboot.controller;
+
+import codegym.vn.springboot.entity.Student;
+import codegym.vn.springboot.form.StudentForm;
+import codegym.vn.springboot.service.ClassNameService;
+import codegym.vn.springboot.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v2/student")
+public class StudentRestController {
+    @Autowired
+    private StudentService studentService;
+
+    @Autowired
+    private ClassNameService classNameService;
+
+    @GetMapping
+    public ResponseEntity<Student> getStudentById(
+            @RequestParam(name = "id") String id) {
+        Student student = studentService.findById(id);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createStudent(@RequestBody Student student) {
+//        Student student = new Student();
+//        student.setId(studentForm.getId());
+//        student.setName(studentForm.getName());
+//        student.setBirthdate(studentForm.getBirthdate());
+//        student.setEmail(studentForm.getEmail());
+//        student.setSex(studentForm.isSex());
+//        student.setPhoneNumber(studentForm.getPhoneNumber());
+//        student.setClassName(classNameService.findById(studentForm.getClassId()));
+        studentService.create(student);
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<Student>> getAllStudent () {
+        List<Student> studentList = studentService.findAll();
+        return new ResponseEntity<>(studentList, HttpStatus.OK);
+    }
+}
